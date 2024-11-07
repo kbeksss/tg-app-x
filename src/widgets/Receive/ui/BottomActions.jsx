4bdaf938687b04a085941430df2fbebe21f3c63c
beks-dyor <bk@dyor.exchange>
2024-11-07 16:13:55 +0600

    handle Share

--- src/widgets/Receive/ui/BottomActions.jsx
+++ src/widgets/Receive/ui/BottomActions.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Box, Button } from '@mui/material'
-import { copyToClipboard } from '@shared/utils/functions'
+import { copyToClipboard, notify } from '@shared/utils/functions'
+import { shareURL } from '@telegram-apps/sdk'
 
 const BottomActions = ({ copyValue }) => {
+    const handleShare = () => {
+        shareURL(copyValue, copyValue)
+        notify({ type: 'success', msg: t('REFERRAL_SEND_SUCCESS') })
+    }
     return (
         <Box sx={{ position: 'fixed', bottom: 85, left: 0, right: 0, px: 2 }}>
             <Button
@@ -12,7 +17,7 @@ const BottomActions = ({ copyValue }) => {
                 variant={'contained'}>
                 Copy
             </Button>
-            <Button fullWidth variant={'outlined'}>
+            <Button onClick={handleShare} fullWidth variant={'outlined'}>
                 Share
             </Button>
         </Box>
