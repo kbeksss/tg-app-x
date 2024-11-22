f1494860242cce625372de0b222840c0d558e67c
beks-dyor <bk@dyor.exchange>
2024-11-22 22:44:10 +0600

    check onButton

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -3,11 +3,24 @@ import { Box, Stack } from '@mui/material'
 import { BottomButton, Search } from '@shared/ui'
 import { UsersList } from '@widgets'
 import { useTg } from '@shared/hooks/useTg.js'
+import { useNavigate } from 'react-router-dom'
 
 const SearchPage = () => {
+    const navigate = useNavigate()
     const [searchQuery, setSearchQuery] = useState('')
     const [isMyList, setIsMyList] = useState(true)
-    useTg({ backButtonVisible: true, onBack: () => alert('back') })
+    useTg({
+        backButtonVisible: true,
+        onBack: () => {
+            if (!isMyList) {
+                setIsMyList(true)
+                console.log('is not mine')
+            } else {
+                console.log('mine')
+                navigate(-1)
+            }
+        },
+    })
     return (
         <Stack sx={{ pt: 2, px: 2, height: 'calc(100vh - 120px)' }}>
             <Search value={searchQuery} setValue={setSearchQuery} />
