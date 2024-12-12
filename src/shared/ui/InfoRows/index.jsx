29a34c932e793b7f2ce77c92554edeac46e968e9
beks-dyor <bk@dyor.exchange>
2024-12-13 00:01:18 +0600

    send confirmation

--- src/shared/ui/InfoRows/index.jsx
+++ src/shared/ui/InfoRows/index.jsx
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react'
 import { Box, Divider, Stack, Typography } from '@mui/material'
+import { useTg } from '@shared/hooks/useTg.js'
 
 const InfoRows = ({ rows }) => {
+    const { isDark } = useTg()
     return (
         <Box
             sx={{
                 borderRadius: '16px',
-                border: '1px solid rgba(0,0,0,0.3)',
+                border: `1px solid ${isDark ? 'rgba(255,255,255,0.3)' : 'rgba(0,0,0,0.3)'}`,
                 py: '20px',
                 px: 2,
             }}>
@@ -20,7 +22,16 @@ const InfoRows = ({ rows }) => {
                             {row?.value}
                         </Typography>
                     </Stack>
-                    {rows.length > index + 1 && <Divider sx={{ my: 2 }} />}
+                    {rows.length > index + 1 && (
+                        <Divider
+                            sx={{
+                                my: 2,
+                                borderColor: isDark
+                                    ? 'rgba(255,255,255,0.3)'
+                                    : 'rgba(0,0,0,0.3)',
+                            }}
+                        />
+                    )}
                 </Fragment>
             ))}
         </Box>
--- src/widgets/Send/ui/SendConfirmDialog.jsx
+++ src/widgets/Send/ui/SendConfirmDialog.jsx
@@ -10,10 +10,22 @@ import {
     Typography,
 } from '@mui/material'
 import { InfoRows } from '@shared/ui'
+import { useTg } from '@shared/hooks/useTg.js'
 
 const SendConfirmDialog = ({ open, onClose, total, address, onConfirm }) => {
+    const { isDark } = useTg()
     return (
-        <Dialog open={open} onClose={onClose}>
+        <Dialog
+            PaperProps={{
+                sx: (theme) => ({
+                    backgroundColor: isDark
+                        ? theme.palette?.darkVersion?.lightBlack
+                        : 'background.paper',
+                    color: isDark ? 'white' : 'unset',
+                }),
+            }}
+            open={open}
+            onClose={onClose}>
             <DialogTitle>Do you confirm?</DialogTitle>
             <DialogContent>
                 <DialogContentText sx={{ pb: 4 }}>
@@ -27,10 +39,26 @@ const SendConfirmDialog = ({ open, onClose, total, address, onConfirm }) => {
                     ]}
                 />
                 <Stack sx={{ mt: 2 }} spacing={1}>
-                    <Button onClick={onConfirm} variant={'contained'}>
+                    <Button
+                        sx={{
+                            backgroundColor: isDark
+                                ? 'darkVersion.green'
+                                : 'primary',
+                            color: isDark ? 'black' : 'white',
+                        }}
+                        onClick={onConfirm}
+                        variant={'contained'}>
                         Confirm
                     </Button>
-                    <Button onClick={onClose} variant={'outlined'}>
+                    <Button
+                        sx={{
+                            backgroundColor: isDark
+                                ? 'darkVersion.lightGrey'
+                                : 'unset',
+                            color: isDark ? 'white' : 'primary',
+                        }}
+                        onClick={onClose}
+                        variant={isDark ? 'contained' : 'outlined'}>
                         No
                     </Button>
                 </Stack>
