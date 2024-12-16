b8e288bd5625734e0dc2b866a83d6a2a9103b4e5
beks-dyor <bk@dyor.exchange>
2024-12-16 18:13:59 +0600

    dark theme

--- src/shared/hooks/useTg.js
+++ src/shared/hooks/useTg.js
@@ -6,9 +6,7 @@ import { COLOR_THEME } from '@shared/config.js'
 const tg = window.Telegram.WebApp
 
 export function useTg(params) {
-    const [isDark, setIsDark] = useState(
-        localStorage.getItem(COLOR_THEME) || tg?.colorScheme || window.darkTheme
-    )
+    const [isDark, setIsDark] = useState(tg?.colorScheme || window.darkTheme)
     const navigate = useNavigate()
     const closeTg = () => {
         tg.close()
