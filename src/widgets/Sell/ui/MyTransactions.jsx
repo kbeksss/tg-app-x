3ebb1ce6f074c5bc7b15edbfff15bc7c3812107d
beks-dyor <bk@dyor.exchange>
2024-12-27 20:46:49 +0600

    my transactions

--- src/widgets/Sell/ui/MyTransactions.jsx
+++ src/widgets/Sell/ui/MyTransactions.jsx
@@ -3,7 +3,7 @@ import { Box, Stack, Typography } from '@mui/material'
 
 const MyTransactions = () => {
     return (
-        <Box>
+        <Box sx={{ py: 3, px: 2 }}>
             <Stack justifyContent={'space-between'}>
                 <Typography>My transactions</Typography>
             </Stack>
